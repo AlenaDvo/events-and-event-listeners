0247893577f4416ff0ef4564312ff3fd11a7caf4
AlenaDvo <131749753+AlenaDvo@users.noreply.github.com>
2023-05-16 17:53:36 +0200

    Changed with addEventListener

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-$('#first').on('click', () => {
+// $('#first').on('click', () => {
+//     console.log('Yeah, you clicked me');
+// });
+
+document.getElementById("first").addEventListener("click", function () {
     console.log('Yeah, you clicked me');
 });
 
-$('#second').on('click', () => {
+// $('#second').on('click', () => {
+//     return $('#first').text('Changed text here');
+// });
+
+document.getElementById("second").addEventListener("click", function () {
     return $('#first').text('Changed text here');
 });
 
-$('#third').on('click', () => {
+// $('#third').on('click', () => {
+//     return $('.button').css('background-color', 'lime');
+// });
+
+document.getElementById("third").addEventListener("click", function () {
     return $('.button').css('background-color', 'lime');
 });
+
+// $('#fourth').one('click', () => {
+//     return $('.button').css('background-color', document.querySelector('input').value);
+// });
+
+document.getElementById("fourth").addEventListener("click", function () {
+    return $('.button').css('background-color', document.querySelector('input').value)
+}, { once: true });
